14efaf70e3ba1323f691efea292e64e29ee8a2ff
Gustavo Soeiro Senise <gustavosooeiro@gmail.com>
2019-03-27 13:19:46 -0300

    Complete auth system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_same_user, only: [:edit, :update]
   # GET /users
   # GET /users.json
   def index
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   # PATCH/PUT /users/1
@@ -83,5 +83,12 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit or delete your own profile"
+        redirect_to root_path
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
